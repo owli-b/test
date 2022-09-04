5a25bc9f5da89ca1ed21eb8a7bec0655b328c845
owli-b <owli.be@gmail.com>
2022-09-04 18:11:23 +0430

    ridam

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let batel = 0;
+setTimeout
+document.querySelector('title').textContent = 'r';
+for (let i = 0; i < 100000000; i++) {
+  batel++;
+}
+document.querySelector('title').textContent = 'ri';
+for (let i = 0; i < 10000; i++) {
+  batel++;
+}
+document.querySelector('title').textContent = 'rid';
+for (let i = 0; i < 10000; i++) {
+  batel++;
+}
+document.querySelector('title').textContent = 'ridi';
+for (let i = 0; i < 10000; i++) {
+  batel++;
+}
+console.log(batel);
